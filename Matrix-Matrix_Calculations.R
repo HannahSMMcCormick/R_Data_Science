108979c52496d4272eca4f9a86478d2d40d22d8c
HannahSMMcCormick <mccormick.141203@gmail.com>
2025-09-14 04:38:40 +0100

    Matrix-Matrix_calculations

---
+++ Matrix-Matrix_Calculations.R
@@ -0,0 +1,22 @@
+# %*%(Matrix multiplication) <> *(Element multiplication)
+
+# The Identity Matrix
+
+a <- matrix(c(1, 2, 2, 1), nrow = 2, ncol = 2, byrow = TRUE)
+print(a)
+
+i <- diag(2)
+print(i)
+
+
+print(i %*% a)
+print(a %*% i)
+
+# Square matrix = n * n
+
+# The matrix inverse =  a^(-1) = (1/(ad - bc))* matrix[d,-b.-c,a]
+#where a normal matrix is matrix[a,b,c,d]
+
+# Singular Matrices - dont have an inverse
+
+#Diagonal and triangle matrices
